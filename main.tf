8cb1f239eb6cfcfec434e3579cfcc0a4d4392ea1
teehoo <toheebadeshina@yahoo.com>
2023-10-06 18:46:22 -0400

    i include security group and attach to instance

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ data "aws_key_pair" "Teehookeypair" {
 resource "aws_instance" "teehoo" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name  = "Teehookeypair"
-  # vpc_security_group_ids = [aws_security_group.SG.id]
+  key_name      = "Teehookeypair"
+  vpc_security_group_ids = [aws_security_group.SG.id]
 
 
   tags = {
@@ -19,17 +19,39 @@ resource "aws_instance" "teehoo" {
   }
 
 }
+resource "aws_security_group" "SG" {
 
-# resource "aws_security_group_rule" "allow_ssh" {
-#   type        = "ingress"
-#   from_port   = 22
-#   to_port     = 22
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-#   # security_group_id = aws_security_group.SG.id
-# }
+  name = "terraform-webserverSG"
+
+
+  ingress {
+    description = "Allow SSH"
+
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
+  }
+
+
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
+
+
 
 output "instance_ip_addr" {
   value       = aws_instance.teehoo.private_ip
   description = "The private IP address of the main server instance."
 }
+
+
